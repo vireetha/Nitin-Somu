f1f6b3aa5af9aba120fa8025d5e86e17aa0bb7b4
vireetha <dudalaviree69@gmail.com>
2024-12-17 06:07:45 +0530

    Type casting

---
+++ Python/Typecasting.py
@@ -0,0 +1,26 @@
+name = "Penguin"
+age = 15
+is_student = True
+weight = 56.8
+print("Name: ", name)
+print("Data Type of Name is: ", type(name))
+
+print("Age: ", age)
+print("Data Type of Age is:  ", type(age))
+
+print("IS STUDENT: ", is_student)
+print("Data Type of IS STUDENT: ", type(is_student))
+
+print("weight: ", weight)
+print("Data TYpe of Weight: ", type(weight))
+
+#Type Casting
+
+print("\n After Type Casting...")
+age = str(age)
+
+print(age)
+print("Data Type Of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data TYpe Of Weight is: ", type(weight))
